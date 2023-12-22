553c6fa2a423c7addf2d6dd30f9dd32992b85a09
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-23 00:03:40 +0530

    Changes

---
+++ index.js
@@ -0,0 +1,81 @@
+let movies =[
+    {
+        mid:1,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:2,
+        name:"Spider-Man No way Home",
+        rating:8.2,
+        poster:"https://cdn.marvel.com/content/1x/spider-mannowayhome_lob_crd_03.jpg",
+        description:"The Multiverse Unleashed."
+    },
+    {
+        mid:3,
+        name:"Thor Ragnarok",
+        rating:7.9,
+        poster:"https://i.pinimg.com/originals/bb/80/6c/bb806c7e693169ef7fc5587f04eda0bd.jpg",
+        description:"Thor is imprisoned on the other side of the universe without his mighty hammer and finds himself in a race against time to get back to Asgard to stop Ragnarok—the destruction of his homeworld and the end of Asgardian civilization—at the hands of an all-powerful new threat, the ruthless Hela."
+    },
+    {
+        mid:4,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:5,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:6,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:7,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:8,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:9,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:10,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+    {
+        mid:11,
+        name:"Avengers infinity War",
+        rating:8.4,
+        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+    },
+]
+
+
