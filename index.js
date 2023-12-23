0ec13e5b683d51b92536ea7e6c9c27a018092616
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-23 16:00:39 +0530

    okie

--- index.js
+++ index.js
@@ -1,209 +1,173 @@
 let movies =[
     {
-        mid:1,
         name:"Avengers infinity War",
         rating:8.4,
         poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
         description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
     },
     {
-        mid:2,
         name:"Spider-Man No way Home",
         rating:8.2,
         poster:"https://cdn.marvel.com/content/1x/spider-mannowayhome_lob_crd_03.jpg",
         description:"The Multiverse Unleashed."
     },
     {
-        mid:3,
         name:"Thor Ragnarok",
         rating:7.9,
         poster:"https://i.pinimg.com/originals/bb/80/6c/bb806c7e693169ef7fc5587f04eda0bd.jpg",
         description:"Thor is imprisoned on the other side of the universe without his mighty hammer and finds himself in a race against time to get back to Asgard to stop Ragnarok—the destruction of his homeworld and the end of Asgardian civilization—at the hands of an all-powerful new threat, the ruthless Hela."
     },
-    {
-        mid:4,
+    {   
         name:"Barbie",
         rating:7.0,
         poster:"https://i.pinimg.com/originals/74/26/4b/74264bae2a10a2b3e1a28b10a3a96599.jpg",
         description:"Barbie, an 11-inch- (29-cm-) tall plastic doll with the figure of an adult woman "
     },
     {
-        mid:5,
         name:"Oppenheimer",
         rating:8.4,
         poster:"https://i.pinimg.com/736x/f3/c7/ea/f3c7ea51c1f8a49579dea5fac3cae460.jpg",
         description:" American theoretical physicist. During the Manhattan Project, Oppenheimer was director of the Los Alamos Laboratory and responsible for the research and design of an atomic bomb. He is often known as the “father of the atomic bomb.”"
     },
     {
-        mid:6,
         name:"LEo",
         rating:6.9,
         poster:"https://www.themoviedb.org/t/p/w220_and_h330_face/gSOVog7ydsaF1YpgAqBqnKYFGY.jpg",
         description:"It's an animated movie"
     },
     {
-        mid:7,
         name:"Ant man and the Wasp",
         rating:8.4,
         poster:"https://i.pinimg.com/564x/1b/4b/fe/1b4bfee2454cae209ad096d309c621cc.jpg",
         description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
     },
     {
-        mid:8,
         name:"Ghosted",
         rating:5.8,
         poster:"https://i.pinimg.com/736x/9b/ea/85/9bea8541f252e96e515e5349dd8d144c.jpg",
         description:"Cole is abducted to Pakistan by arms dealers who believe him to be legendary CIA operative Before he can be tortured with insects to extract a passcode, he is rescued by Sadie, who reveals herself to be the Taxman."
     },
     {
-        mid:9,
         name:"John Wick Chapter 4",
         rating:7.7,
         poster:"https://i.pinimg.com/736x/5b/c6/2a/5bc62a7bcd1ca0e3b755ad8756857802.jpg",
         description:"In New York City, John Wick prepares to exact vengeance against the High Table while hiding underground with the Bowery King. He travels to Morocco and kills the Elder."
     },
     {
-        mid:10,
         name:"Joe",
         rating:8.2,
         poster:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/joe-2023-et00376793-1700658988.jpg",
         description:"The romance of a young man who is in love with his college mate ends in heartbreak."
     },
     {
-        mid:11,
         name:"Friends",
         rating:8.9,
         poster:"https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
         description:"Friends is an American television sitcom created by David Crane and Marta Kauffman, which aired on NBC"
     },
     {
-        mid:12,
         name:"Captain america: The Winter Soldier",
         rating:7.9,
         poster:"https://i.pinimg.com/originals/03/f4/f1/03f4f193432f52fa941526ffe6461e50.jpg",
         description:" The Winter Soldier is a 2014 American superhero film based on the Marvel Comics character Captain America"
     },
     {
-        mid:13,
         name:"Game of thrones",
         rating:9.2,
         poster:"https://i.pinimg.com/736x/d4/af/32/d4af32dda6440ae6eae3ade9a2dca53e.jpg",
         description:"The first story arc follows a war of succession among competing claimants for control of the Iron Throne of the Seven Kingdoms, with other noble families fighting for independence from the throne."
     },
     {
-        mid:14,
         name:"Peaky Blinders",
         rating:8.8,
         poster:"https://i.pinimg.com/1200x/c2/e2/9b/c2e29b9f6982875311d562935c22123e.jpg",
         description:"Tommy Shelby, a dangerous man, leads the Peaky Blinders, a gang based in Birmingham. Soon, Chester Campbell, an inspector, decides to nab him and put an end to the criminal activities."
     },
     {
-        mid:15,
         name:"Money Heist",
         rating:8.2,
         poster:"https://i.pinimg.com/474x/0e/aa/ec/0eaaec8f03859575a7a6883f5256e351.jpg",
         description:"This riveting crime series won Best Drama at the International Emmy Awards, Premios Fénix and Premios Iris "
     },
     {
-        mid:16,
         name:"Breaking Bad",
         rating:9.5,
         poster:"https://i.pinimg.com/originals/ef/57/c9/ef57c9b3bcbf4429306bdc7bf4ab672d.jpg",
         description:"Walter White, a chemistry teacher, discovers that he has cancer and decides to get into the meth-making business to repay his medical debts. His priorities begin to change when he partners with Jesse."
     },
     {
-        mid:17,
         name:"All of us are dead",
         rating:7.5,
         poster:"https://i.pinimg.com/736x/bc/04/11/bc041148ebee2e1fe226519c12ee84d4.jpg",
         description:"Trapped students must escape their high school which has become ground zero for a zombie virus outbreak."
     },
     {
-        mid:18,
         name:"The Boys",
         rating:8.7,
         poster:"https://i.pinimg.com/736x/a1/9f/a5/a19fa5421596a8efd3855e342c5f9093.jpg",
         description:"Superheroes are often as popular as celebrities, as influential as politicians, and sometimes even as revered as gods."
     },
     {
-        mid:19,
         name:"Cobra Kai",
         rating:8.5,
         poster:"https://i.pinimg.com/736x/93/b0/b4/93b0b488e9f50ac86bf0c66b75016ac0.jpg",
         description:"Thirty four years after events of the 1984 All Valley Karate Tournament, a down-and-out Johnny Lawrence seeks redemption by reopening the infamous Cobra Kai dojo, reigniting his rivalry with a now successful Daniel LaRusso."
     },
     {
-        mid:20,
         name:"Sex Education",
         rating:8.3,
         poster:"https://i.pinimg.com/originals/61/df/03/61df03efeff974b1017ea1c479e79fe8.jpg",
         description:"Socially awkward high school student Otis may not have much experience in the lovemaking department, but he gets good guidance on the topic in his personal sex ed course "
     },
     {
-        mid:21,
         name:"Daily dose of sunshine",
         rating:8.3,
         poster:"https://i.pinimg.com/1200x/2e/11/26/2e1126a1f611ad2d3bdeb0992a6f79c3.jpg",
         description:"A kind-hearted nurse working in psychiatry goes above and beyond to be a daily dose of sunshine for those under her care despite the challenges coming her way."
     },
     {
-        mid:22,
         name:"Extraordinary YOU",
         rating:7.8,
         poster:"https://i.pinimg.com/236x/79/1e/6c/791e6cff1e6013796e539dc62a66263e.jpg",
         description:"When a high-school girl finds that she is merely a character from a comic book whose destiny is decided by the writer, she decides to change the plot to suit her desires and find the love of her life."
     },
     {
-        mid:23,
         name:"Never the less",
         rating:7.1,
         poster:"https://i.pinimg.com/736x/39/4b/60/394b60dd4a63edac25154e6a298399fc.jpg",
         description:"The story of a romance between a man who is annoyed with relationships but likes to flirt and a woman who wants to date but does not believe in love."
     },
     {
-        mid:24,
         name:"Revenge of others",
         rating:7.6,
         poster:"https://i.pinimg.com/736x/21/d3/e2/21d3e2f9c46c19fbb05ccf6c95ccb8f3.jpg",
         description:"Chanmi transfers to her brother's school, Yongtan High; a hero appears at Yongtan High, and Chanmi speculates that it may be connected to her brother."
     },
     {
-        mid:25,
-        name:"Business Proposal",
-        rating:8.1,
-        poster:"https://i.pinimg.com/736x/5d/3f/9a/5d3f9ad6b33572e85d0c89ee2e92d486.jpg",
-        description:"In disguise as her friend, Ha-ri shows up on a blind date to scare away her friend's prospective suitor. However, plans go awry when he turns out to be Ha-ri's CEO and he makes a proposal."
-    },
-    {
-        mid:26,
         name:"18 again",
         rating:8.2,
         poster:"https://i.pinimg.com/564x/89/68/d1/8968d1cd5eb8acb840ad2f1b27d01c17.jpg",
         description:"A 37-year-old Dae-young is on the verge of being divorced with Da-jung finds himself inside his 18-year-old body."
     },
     {
-        mid:27,
         name:"It is okay to be not okay",
         rating:8.6,
         poster:"https://i.pinimg.com/236x/6a/17/e6/6a17e6f347ba6735cf0fee2f6b741aeb.jpg",
         description:"A road to emotional healing opens up for an antisocial children's book author and an employee in a psychiatric hospital."
     },
     {
-        mid:28,
         name:"Prison Break",
         rating:8.3,
         poster:"https://i.pinimg.com/originals/0c/9e/c9/0c9ec9a4b81691b36e254a0cc7d066fa.jpg",
         description:"Michael Scofield finds himself in the Ogygia Prison in Sana'a, Yemen, seven years after his apparent death. His friends, brother and fellow escapee do everything it takes to bring him home."
     },
     {
-        mid:29,
         name:"The legend of Blue Sea",
         rating:8.1,
         poster:"https://i.pinimg.com/236x/6e/15/9d/6e159dd15382859b742e29b272f908da.jpg",
         description:"A mermaid, Se-hwa, starts following a con man, Joon-jae, who initially helps her out. However, while she tries to adapt to the human world, she finds herself falling in love with him."
     },
     {
-        mid:30,
         name:"Guardian: The Lonely and Great God",
         rating:8.6,
         poster:"https://i.pinimg.com/736x/2f/0a/f6/2f0af6e69ca3067618e56a32bc72d429.jpg",
@@ -212,3 +176,61 @@ let movies =[
 ]
 
 
+function searchMovie(){
+    let movieName = document.getElementById("search").value;
+    if(movieName!== ""){
+        let result = movies.filter((movie)=>{
+            return movie.name.toUpperCase().includes(movieName.toUpperCase())
+        })
+        // console.log(result)
+        displayMovies(result)
+    }
+    else{
+        displayMovies(movies)
+    }
+    
+}
+
+function displayMovies(data){
+    document.getElementById("movies").innerHTML = "";
+    let htmlString = `
+        
+    `
+    for (let i=0;i<data.length;i++){
+        // console.log(movies[i].name)
+        htmlString = htmlString + `
+            <div class="movie">
+                <div class="overlay">
+                    <div class="video">
+                        
+                    </div>
+                    <div class="details">
+                        <h1>Name: ${data[i].name}</h1>
+                        <h2>IMDB: ${data[i].rating}</h2>
+                        <p>Desc: ${data[i].description}</p>
+                    </div>
+                </div>
+                <img src=${data[i].poster} class="poster" alt="POster">
+            </div>
+        `
+    }
+    document.getElementById("movies").innerHTML = htmlString
+}
+displayMovies(movies)
+
+
+
+
+
+
+
+
+{/* <div></div> */}
+// function displayMovies(){
+//     let movie = document.createElement("div");
+//     movie.classList.add('movie');
+
+//     let overlay = document.createElement("div");
+//     overlay.classList.add('overlay')
+//     movie.appendChild(overlay)    // Parent div appends Child div
+// }
