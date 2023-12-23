4044fdfd63d75c466f669c53094ae833af9bc0eb
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-23 10:04:16 +0530

    changes

--- index.js
+++ index.js
@@ -22,30 +22,30 @@ let movies =[
     },
     {
         mid:4,
-        name:"Avengers infinity War",
-        rating:8.4,
-        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
-        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+        name:"Barbie",
+        rating:7.0,
+        poster:"https://i.pinimg.com/originals/74/26/4b/74264bae2a10a2b3e1a28b10a3a96599.jpg",
+        description:"Barbie, an 11-inch- (29-cm-) tall plastic doll with the figure of an adult woman "
     },
     {
         mid:5,
-        name:"Avengers infinity War",
+        name:"Oppenheimer",
         rating:8.4,
-        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
-        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+        poster:"https://i.pinimg.com/736x/f3/c7/ea/f3c7ea51c1f8a49579dea5fac3cae460.jpg",
+        description:" American theoretical physicist. During the Manhattan Project, Oppenheimer was director of the Los Alamos Laboratory and responsible for the research and design of an atomic bomb. He is often known as the “father of the atomic bomb.”"
     },
     {
         mid:6,
-        name:"Avengers infinity War",
-        rating:8.4,
-        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
-        description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
+        name:"LEo",
+        rating:6.9,
+        poster:"https://www.themoviedb.org/t/p/w220_and_h330_face/gSOVog7ydsaF1YpgAqBqnKYFGY.jpg",
+        description:"It's an animated movie"
     },
     {
         mid:7,
-        name:"Avengers infinity War",
+        name:"Ant man and the Wasp",
         rating:8.4,
-        poster:"https://cdn.europosters.eu/image/750/plastic-frame-avengers-infinity-war-one-sheet-i69678.jpg",
+        poster:"https://i.pinimg.com/564x/1b/4b/fe/1b4bfee2454cae209ad096d309c621cc.jpg",
         description:"On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality."
     },
     {
